36d2e50c66f5458b6c5f6e66791cb8be50c1d8e2
Emma <eeg4@uw.edu>
2021-12-09 00:02:15 -0800

    Added store.js Util

---
+++ client/src/utils/store.js
@@ -0,0 +1,8 @@
+// Creates a Redux store that holds the staet of the app.
+import { createStore } from 'redux';
+
+// If errors, try importing reducer function directly
+import reducers from './reducers';
+
+
+export default createStore(reducers);
